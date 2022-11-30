d4ebb5acdd24347eea4dcdd333069667496e12ca
Łukasz Szmyd <szmydlukasz30@gmail.com>
2022-11-30 10:48:21 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/multi-step-form/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
